66726aa5c13ade3ff42f12ac2ba27c4a601f02d6
keshav-1225 <keshaav.mishra2006@gmail.com>
2025-06-24 17:27:10 +0530

    Add Comments

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let currentSong = new Audio();  //It is used to play the current track so that when a song is changed, new track starts playing instead of overlapping and playing both the songs
-async function getSongs() { //This function creates an array of songs fetched in the songs folder.
-    const dir = await fetch('http://127.0.0.1:3000/songs/');
-    let response = await dir.text();    // it converts the html into text
+// ========== Audio Player Setup ==========
+let currentSong = new Audio();  // Used to play the current track and prevent overlapping songs
 
+// ========== Fetch Songs from Server ==========
+async function getSongs() {
+    // Fetch the list of songs from the server directory
+    const dir = await fetch('http://127.0.0.1:3000/songs/');
+    let response = await dir.text();    // Convert HTML response to text
+
+    // Parse the HTML to extract song links
     let div = document.createElement("div");
     div.innerHTML = response;
-
     let as = div.getElementsByTagName("a");
 
     songs = []
@@ -18,25 +22,31 @@ async function getSongs() { //This function creates an array of songs fetched in
     return songs
 }
 
-function displaySongs(songs) {  //It displays the song in the sidebar
+// ========== Display Songs in Sidebar ==========
+function displaySongs(songs) {
     let displaySongsPlaylist = document.querySelector(".playlist-display").getElementsByTagName("ul")[0];
     for (let song of songs) {
         song = song[1].replaceAll("%20", " ")
         song = song.replaceAll("%C2%A3%C3%BC", "<span hidden>%C2%A3%C3%BC</span>")
         displaySongsPlaylist.innerHTML += `<li>
                             <img src="images/music.svg" alt="music">
-                            <div class="song">${song}</div>
+                            <div class="song hover">${song}</div>
                             <img src="images/play.svg" alt="play now" class="hover">
                         </li>`
     }
 }
-function PlaySong(track, pause = true) {   //It plays the song whenever a song is selected
-    // let audio = new Audio("/songs/"+track);
+
+// ========== Play Selected Song ==========
+function PlaySong(track, pause = true) {
+    // Play the selected song and update play/pause button
     currentSong.src = "/songs/" + track;
     currentSong.play()
     document.querySelector(".playpause").innerHTML = `<img src="images/pause.svg" alt="Play/Pause button">`;
 }
-function updateTimer(currentTime, duration) {   //It is used to showcase duration of the song. Created by github copilot
+
+// ========== Update Song Timer ==========
+function updateTimer(currentTime, duration) {
+    // Format seconds to mm:ss
     function formatTime(sec) {
         if (isNaN(sec)) return "00:00";
         let minutes = Math.floor(sec / 60);
@@ -45,19 +55,24 @@ function updateTimer(currentTime, duration) {   //It is used to showcase duratio
     }
     document.querySelector(".timer").textContent = `${formatTime(currentTime)}/${formatTime(duration)}`;
 }
+
+// ========== Main App Logic ==========
 async function main() {
     let play = document.querySelector(".playpause");
     let songs = await getSongs()
     let display = displaySongs(songs);
     let selectedsong = Array.from(document.querySelectorAll(".song"));
     let displaySongName = document.querySelector(".songinfo");
+
+    // Add click event to each song in the sidebar
     selectedsong.forEach(element => {
         element.addEventListener("click", () => {
             displaySongName.textContent = element.textContent.replaceAll("%C2%A3%C3%BC", " ");
             PlaySong(element.textContent.trim(), false)
         })
     });
-    console.log(play.innerHTML)
+
+    // Play/Pause button logic
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play()
@@ -67,6 +82,8 @@ async function main() {
             play.innerHTML = `<img src="images/play-button.svg" alt="Play/Pause button">`
         }
     })
+
+    // Update seekbar and timer as song plays
     currentSong.addEventListener("timeupdate", () => {
         updateTimer(currentSong.currentTime, currentSong.duration);
         let percent = 0;
@@ -76,8 +93,9 @@ async function main() {
         document.querySelector(".dot").style.left = `${percent}%`;
         document.querySelector(".seekbar-fill").style.width = `${percent}%`;
     })
+
+    // Seekbar click: jump to position in song
     document.querySelector(".seekbar").addEventListener("click", e => {
-        // console.log(`offset x = ${e.offsetX} \noffset Y = ${e.offsetY} \nScreen X = ${e.screenX} \nScreen Y = ${e.screenY}`);
         let seekbar = e.currentTarget;
         let rect = seekbar.getBoundingClientRect();
         let offsetX = e.clientX - rect.left;
@@ -86,6 +104,8 @@ async function main() {
         document.querySelector(".seekbar-fill").style.width = `${seekbarClick}%`;
         currentSong.currentTime = (currentSong.duration)*seekbarClick/100;
     })
+
+    // Sidebar close (cross icon)
     document.querySelector(".cross-icon").addEventListener("click",e=>{
         document.querySelector(".sidebar").classList.add("hideSidebar")
         // document.querySelector(".mainwindow").classList.add("width100")
